75703ff2efbbfe7c9b01c6936f7c8f50e20b77a4
Rupesh Khadka <khadkarupesh818@gmail.com>
2024-07-20 19:52:23 +0400

    grt

--- Frontend/synergy/src/components/Bar.jsx
+++ Frontend/synergy/src/components/Bar.jsx
@@ -7,74 +7,102 @@ import Button from "react-bootstrap/Button";
 import Login from "../assets/login.svg";
 import "../scss/nav.scss";
 import Logo from "../assets/logo-color.png";
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
 const Bar = () => {
-  const expand = 'xxl';
+  const expand = "sm";
   return (
-    <div className="main  align-items-center">
-      <Container className="contain">
-        <Row className="cont d-flex align-items-center">
-          <Col xs={12} sm={12} md={4} className="im">
-            <img src={Logo} className="img d-flex align-items-center" alt="Logo" />
-          </Col>
-          <Col xs={12} sm={12} md={4} className="order-md-3 login d-flex">
-            <img src={Login} alt="Login" />
-          </Col>
-          <Col xs={12} sm={12} md={4} className="order-md-2 d-flex align-items-center search-cont">
-            <Form inline className="search-container">
-              <Form.Control
-                type="text"
-                placeholder="Search"
-                className="mr-sm-2 search"
+    <>
+      <div className="main  align-items-center">
+        <Container className="contain">
+          <Row className="cont d-flex align-items-center">
+            <Col xs={12} sm={12} md={4} className="im">
+              <img
+                src={Logo}
+                className="img d-flex align-items-center"
+                alt="Logo"
               />
-              <Button className="btn search-btn" type="submit">
-                Search
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-        <Navbar expand={expand} fixed="bottom" className="bg-body-tertiary">
-      <Container fluid>
-        
-        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-        <Navbar.Offcanvas
-          id={`offcanvasNavbar-expand-${expand}`}
-          aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-          placement="end"
-        >
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-              Offcanvas
-            </Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <Nav className="justify-content-start flex-grow-1 pe-3">
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-              <NavDropdown
-                title="Dropdown"
-                id={`offcanvasNavbarDropdown-expand-${expand}`}
-              >
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
-                  Something else here
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Offcanvas.Body>
-        </Navbar.Offcanvas>
-      </Container>
-      </Navbar>
-      </Container>
-    </div>
+            </Col>
+            <Col xs={12} sm={12} md={4} className="order-md-3 login d-flex">
+              <img src={Login} alt="Login" />
+            </Col>
+            <Col
+              xs={12}
+              sm={12}
+              md={4}
+              className="order-md-2 d-flex align-items-center search-cont"
+            >
+              <Form inline className="search-container">
+                <Form.Control
+                  type="text"
+                  placeholder="Search"
+                  className="mr-sm-2 search"
+                />
+                <Button className="btn search-btn" type="submit">
+                  Search
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="second-nav d-flex align-items-center">
+        <Container>
+          <Row className="d-flex align-items-center">
+            <Col>
+              <Navbar expand={expand} className="se">
+                <Navbar.Toggle
+                  aria-controls={`offcanvasNavbar-expand-${expand}`}
+                />
+                <Navbar.Offcanvas
+                  id={`offcanvasNavbar-expand-${expand}`}
+                  aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                  placement="end"
+                >
+                  <Offcanvas.Header closeButton>
+                    <Offcanvas.Title
+                      id={`offcanvasNavbarLabel-expand-${expand}`}
+                    >
+                      Offcanvas
+                    </Offcanvas.Title>
+                  </Offcanvas.Header>
+                  <Offcanvas.Body>
+                    <Nav className="nav2">
+                      <Nav.Link href="#action1">Home</Nav.Link>
+                      <Nav.Link href="#action2">Services</Nav.Link>
+                      <NavDropdown
+                        title="Company Profile"
+                        id={`offcanvasNavbarDropdown-expand-${expand}`}
+                      >
+                        <NavDropdown.Item href="#action3">
+                          Action
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">
+                          Another action
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action5">
+                          Something else here
+                        </NavDropdown.Item>
+                      </NavDropdown>
+                      <Nav.Link href="#action6">Products</Nav.Link>
+                      <Nav.Link href="#action7">Certification</Nav.Link>
+                      <Nav.Link href="#action9">About Us</Nav.Link>
+                      <Nav.Link href="#action2">Contact Us</Nav.Link>
+                    </Nav>
+                  </Offcanvas.Body>
+                </Navbar.Offcanvas>
+              </Navbar>
+            </Col>
+            <Col className="d-flex align-items-center">
+      
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
   );
 };
 
--- Frontend/synergy/src/components/Body.jsx
+++ Frontend/synergy/src/components/Body.jsx
@@ -7,34 +7,45 @@ import Pic from "../assets/1.jpg";
 import "../scss/body.scss";
 
 const Body = () => {
-  return <>
-  <div className="bod">
-    <Container>
-        <Row>
+  return (
+    <>
+      <div className="bod">
+        <Container>
+          <Row>
             <Col sm={6} className="text-center">
-            <h1>DIVE DEEPER WITH</h1>
-            <h1>SYNERGY</h1>
-            <h5>"Innovation Meets Excellence"</h5>
-            <p>Synergy, a diving equipment leader since 2008, offers innovative, top-quality products. From a small warehouse to a state-of-the-art facility, we ensure every dive is safe, efficient, and 
-                successful. Dive deeper with Synergy.</p>
+              <h1 className="h1">DIVE DEEPER WITH</h1>
+              <h1>SYNERGY!</h1>
+              <p>
+                Synergy, a diving equipment leader since 2008, offers
+                innovative, top-quality products. From a small warehouse to a
+                state-of-the-art facility, we ensure every dive is safe,
+                efficient, and successful.
+              </p>
+              <Col className="but">
+              <Button className="btn">Get Quotations</Button>
             </Col>
-            <Col sm={6} className="text-center1"><img src={Pic} alt="" /></Col>
-            <Col className="but">
-            <Button className="btn">Contact us</Button>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-          <h1>Trusted by 150+ enterprises, scale-ups and startups.</h1>
-          <p>
-          At Leapfrog, we have had the privilege of partnering with some of the most 
-          innovative and forward-thinking companies in the tech industry. From big companies
-           to startups that hope to be big one day, they trust us with the expertise and 
-           support they need to bring their ideas to life.</p></Col>
-        </Row>
-    </Container>
-  </div>
-  </>;
+            </Col>
+           
+            <Col sm={6} className="text-center1">
+              <img src={Pic} alt="" />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h1>Trusted by 150+ enterprises, scale-ups and startups.</h1>
+              <p>
+                At Leapfrog, we have had the privilege of partnering with some
+                of the most innovative and forward-thinking companies in the
+                tech industry. From big companies to startups that hope to be
+                big one day, they trust us with the expertise and support they
+                need to bring their ideas to life.
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
 };
 
 export default Body;
