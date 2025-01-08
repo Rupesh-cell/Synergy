e6fc3516c9793340c791c510c1b353ff97a97b62
Rupesh Khadka <khadkarupesh818@gmail.com>
2025-01-08 14:15:17 +0400

    comit

--- Frontend/synergy/src/components/Body.jsx
+++ Frontend/synergy/src/components/Body.jsx
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/esm/Button";
-import Pic from "../assets/1.png";
+import Pic from "../assets/1.webp";
 import "../scss/body.scss";
 
 const Body = () => {
@@ -12,9 +12,12 @@ const Body = () => {
       <div className="bod">
         <Container>
           <Row>
+            {/* <Col sm={6} className="text-center1">
+              <img src={Pic} alt="" />
+            </Col> */}
             <Col sm={6} className="text-center">
-              <h1 className="h1">SynergyUnilok:</h1>
-              <h1>Connecting Innovation with Excellence!</h1>
+              <h1 className="h1">DIVE DEEPER WITH</h1>
+              <h1>SYNERGY!</h1>
               <p>
                 Synergy, a diving equipment leader since 2008, offers
                 innovative, top-quality products. From a small warehouse to a
@@ -22,12 +25,8 @@ const Body = () => {
                 efficient, and successful.
               </p>
               <Col className="but">
-              <Button className="btn">Get Quotations</Button>
-            </Col>
-            </Col>
-           
-            <Col sm={6} className="text-center1">
-            <img src={Pic} alt="" />
+                <Button className="btn">Get Quotations</Button>
+              </Col>
             </Col>
           </Row>
         </Container>
